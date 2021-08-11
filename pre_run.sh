0c5634660b3233a8665a5105e174a4e14a9a3285
vamshikrishna <everyoursvamshi@gmail.com>
2021-08-11 21:22:49 +0530

    7th commit
    Signed-off-by: vamshikrishna <everyoursvamshi@gmail.com>

--- pre_run.sh
+++ pre_run.sh
@@ -1,5 +1,8 @@
 pwd
 LAST_SUCCESSFUL_COMMIT=$(curl -Ss -u "$API_KEY:" "https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME?filter=successful&limit=1" | jq -r '.[0]["vcs_revision"]')
+git clone  "$CIRCLE_REPOSITORY_URL" --branch "$CIRCLE_BRANCH" test1
+cd test1
+git reset --hard $LAST_SUCCESSFUL_COMMIT
 echo $LAST_SUCCESSFUL_COMMIT
 echo $CIRCLE_SHA1
 echo "Thanks"
